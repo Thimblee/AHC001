2a798a27e6df9c934f06ba3e40d2a4292e512daa
Thimblee <Thimblee@users.noreply.github.com>
2021-03-11 15:14:33 +0900

    top-left to bottom-right 4589

--- topLeft.cpp
+++ topLeft.cpp
@@ -22,6 +22,10 @@ void paintOut(int tl_x, int tl_y, int br_x, int br_y) {
     }
 }
 
+int area(int tl_x, int tl_y, int br_x, int br_y) {
+    return (br_x - tl_x + 1) * (br_y - tl_y + 1);
+}
+
 vector<int> topLeft(int sx, int sy, int r) {
 
     bool left_f = true, top_f = true;
@@ -30,6 +34,9 @@ vector<int> topLeft(int sx, int sy, int r) {
     while (left_f || top_f) {
 
         if (now_col == 0) left_f = false;
+        if (area(now_col-1, now_row, sx, sy) > r) {
+            left_f = false;
+        }
         if (left_f) {
             for (int i = now_row; i <= sy; ++i) {
                 if (flgs[i][now_col-1]) {
@@ -38,9 +45,12 @@ vector<int> topLeft(int sx, int sy, int r) {
                 }
             }
             if (left_f) now_col--;
-        }
+        }   
 
         if (now_row == 0) top_f = false;
+        if (area(now_col, now_row-1, sx, sy) > r) {
+            top_f = false;
+        }
         if (top_f) {
             for (int i = now_col; i <= sx; ++i) {
                 if (flgs[now_row-1][i]) {
@@ -51,7 +61,42 @@ vector<int> topLeft(int sx, int sy, int r) {
             if (top_f) now_row--;
         }
     }
-    return {now_col, now_row};
+
+    bool right_f = true, bottom_f = true;
+    int ty = now_row, tx = now_col;
+    now_row = sy, now_col = sx;
+
+    while (right_f || bottom_f) {
+        
+        if (now_col == MAX-1) right_f = false;
+        if (area(tx, ty, now_col+1, now_row) > r) {
+            right_f = false;
+        }
+        if (right_f) {
+            for (int i = ty; i <= now_row; ++i) {
+                if (flgs[i][now_col+1]) {
+                    right_f = false;
+                    break;
+                }
+            }
+            if (right_f) now_col++;
+        }
+
+        if (now_row == MAX-1) bottom_f = false;
+        if (area(tx, ty, now_col, now_row+1) > r) {
+            bottom_f = false;
+        }
+        if (bottom_f) {
+            for (int i = tx; i <= now_col; ++i) {
+                if (flgs[now_row+1][i]) {
+                    bottom_f = false;
+                    break;
+                }
+            }
+            if (bottom_f) now_row++;
+        }
+    }
+    return {tx, ty, now_col, now_row};
 }
 
 void solve() {
@@ -60,8 +105,8 @@ void solve() {
     rep(i, n) {
         int x = xyr[i][1], y = xyr[i][2], r = xyr[i][3];
         vector<int> top_left = topLeft(x, y, r);
-        paintOut(top_left[0], top_left[1], x, y);
-        res[xyr[i][0]] = {top_left[0]+1, top_left[1]+1, x+1, y+1};
+        paintOut(top_left[0], top_left[1], top_left[2], top_left[3]);
+        res[xyr[i][0]] = {top_left[0], top_left[1], top_left[2] + 1, top_left[3] + 1};
     }
 
     rep(i, n) {
